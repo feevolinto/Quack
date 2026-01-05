ca849e67a70316311ca38397abd660df0b307a7a
pebble_na_bato <feevolinto123@gmail.com>
2026-01-06 00:16:57 +0800

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,8 @@
 import app from "./app.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import boardRoutes from "./routes/board.routes.js";
+import taskRoutes from "./routes/task.routes.js";
 
 dotenv.config();
 
@@ -16,3 +18,7 @@ mongoose
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
+app.use("/api/boards", boardRoutes);
+app.use("/api/tasks", taskRoutes);
+
