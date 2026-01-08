0c7c3a15491c1d889b605164f7d3827f99416d0f
pebble_na_bato <feevolinto123@gmail.com>
2026-01-08 22:57:05 +0800

    Update User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,25 +1,20 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    role: {
-      type: String,
-      enum: ["admin", "member"],
-      required: true
-    }
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
   },
-  { timestamps: true }
-);
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user"
+  }
+}, { timestamps: true });
 
-const User = mongoose.model("User", UserSchema);
-
-export default User;
+export default mongoose.model("User", userSchema);
