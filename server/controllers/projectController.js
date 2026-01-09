ecb62ef23b01900ee6814c748fe2a3c038bfbd71
pebble_na_bato <feevolinto123@gmail.com>
2026-01-09 15:27:13 +0800

    Update projectController.js

---
+++ server/controllers/projectController.js
@@ -0,0 +1,30 @@
+import Project from "../models/Project.js";
+
+// CREATE project
+export const createProject = async (req, res) => {
+  try {
+    const project = await Project.create({
+      name: req.body.name,
+      description: req.body.description,
+      owner: req.user.id,
+      members: [req.user.id]
+    });
+
+    res.status(201).json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// GET my projects
+export const getMyProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({
+      members: req.user.id
+    });
+
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
