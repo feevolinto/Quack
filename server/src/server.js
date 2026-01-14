d6aa227133631130352fbb9d215b7d219f1448f0
pebble_na_bato <feevolinto123@gmail.com>
2026-01-14 22:28:16 +0800

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,36 @@
+// import app from "./app.js";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import boardRoutes from "./routes/board.routes.js";
+// import taskRoutes from "./routes/task.routes.js";
+// import authRoutes from "./routes/auth.routes.js";
+// import projectRoutes from "./routes/project.routes.js";
+
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB error:", err);
+//   });
+
+// // ROUTES FIRST
+// app.use("/api/boards", boardRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/projects", projectRoutes);
+
+// // THEN LISTEN
+// app.listen(5000, () => {
+//   console.log("Server running on port 5000");
+// });
+
 import app from "./app.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import boardRoutes from "./routes/board.routes.js";
-import taskRoutes from "./routes/task.routes.js";
-import authRoutes from "./routes/auth.routes.js";
-import projectRoutes from "./routes/project.routes.js";
-
 
 dotenv.config();
 
@@ -21,9 +46,3 @@ mongoose
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
-
-app.use("/api/boards", boardRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
-
