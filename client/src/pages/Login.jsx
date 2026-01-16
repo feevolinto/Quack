2d3ef7a0de86ae437063cbbb46b489c30d9459b5
pebble_na_bato <feevolinto123@gmail.com>
2026-01-16 21:47:02 +0800

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,63 +1,32 @@
-import { useState } from "react";
-import api from "../api/axios";
+import { useState, useContext } from "react";
+import { loginRequest } from "../api/auth.api";
+import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [role, setRole] = useState("user");
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const res = await api.post("/auth/login", {
-        email,
-        password,
-        role,
-      });
 
-      localStorage.setItem("token", res.data.token);
-      navigate("/dashboard");
+    try {
+      const res = await loginRequest({ email, password });
+      login(res.data.token, res.data.user);
+      navigate("/");
     } catch (err) {
-      alert(err.response?.data?.message || "Login failed");
+      alert("Login failed");
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <form onSubmit={handleSubmit} className="p-6 border rounded space-y-4">
-        <h1 className="text-xl font-bold">Login</h1>
-
-        <input
-          className="border p-2 w-full"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <input
-          className="border p-2 w-full"
-          placeholder="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <select
-          className="border p-2 w-full"
-          value={role}
-          onChange={(e) => setRole(e.target.value)}
-        >
-          <option value="user">User</option>
-          <option value="admin">Admin</option>
-        </select>
-
-        <button className="bg-black text-white p-2 w-full">
-          Login
-        </button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
