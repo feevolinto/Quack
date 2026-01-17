47822327f471ad7a28df3d70aca87d0b49cb22b4
pebble_na_bato <feevolinto123@gmail.com>
2026-01-17 23:12:12 +0800

    Update History.jsx

---
+++ client/src/pages/History.jsx
@@ -0,0 +1,11 @@
+// src/pages/History.jsx
+function History() {
+  return (
+    <div>
+      <h1 style={{ color: '#e8ff00', marginBottom: '1rem' }}>History</h1>
+      <p>Completed projects will appear here</p>
+    </div>
+  );
+}
+
+export default History;
