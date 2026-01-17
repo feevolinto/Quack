5d6ae984ed876c5eeec4aa988b4e839142a8e2c2
pebble_na_bato <feevolinto123@gmail.com>
2026-01-17 23:12:15 +0800

    Update Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// src/pages/Home.jsx
+function Home() {
+  return (
+    <div>
+      <h1 style={{ color: '#e8ff00', marginBottom: '1rem' }}>Home Page</h1>
+      <p>Welcome to Quack Project Management</p>
+    </div>
+  );
+}
+
+export default Home;
