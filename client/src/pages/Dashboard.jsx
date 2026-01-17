5933e558b20674bf8f0318dd694be4fe9f745f22
pebble_na_bato <feevolinto123@gmail.com>
2026-01-17 23:12:10 +0800

    Update Dashboard.jsx

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+// src/pages/Dashboard.jsx
+function Dashboard() {
+  return (
+    <div>
+      <h1 style={{ color: '#e8ff00', marginBottom: '1rem' }}>Dashboard</h1>
+      <p>Project list will appear here</p>
+    </div>
+  );
+}
+
+export default Dashboard;
