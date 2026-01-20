eb465a74dd82e40b7e9a3f558a932f421802a802
pebble_na_bato <feevolinto123@gmail.com>
2026-01-20 22:55:20 +0800

    Create mockData.js

---
+++ client/src/utils/mockData.js
@@ -0,0 +1,64 @@
+// src/utils/mockData.js
+
+export const mockTeam = {
+  name: "AWS Cloud Club UP Mindanao",
+  role: "Team"
+};
+
+export const mockStats = {
+  totalDeliverables: 50,
+  todo: 25,
+  inProgress: 10,
+  finished: 15
+};
+
+export const mockProjects = [
+  {
+    id: 1,
+    name: "Event Name: Lorem ipsum",
+    date: "January 10, 2026",
+    location: "123 Lorem ipsum dolor sit amet, Davao City, 8000",
+    progress: 21,
+    daysRemaining: 10
+  },
+  {
+    id: 2,
+    name: "Event Name: Lorem ipsum",
+    date: "January 10, 2026",
+    location: "123 Lorem ipsum dolor sit amet, Davao City, 8000",
+    progress: 21,
+    daysRemaining: 10
+  },
+  {
+    id: 3,
+    name: "Event Name: Lorem ipsum",
+    date: "January 10, 2026",
+    location: "123 Lorem ipsum dolor sit amet, Davao City, 8000",
+    progress: 21,
+    daysRemaining: 10
+  },
+  {
+    id: 4,
+    name: "Event Name: Lorem ipsum",
+    date: "January 10, 2026",
+    location: "123 Lorem ipsum dolor sit amet, Davao City, 8000",
+    progress: 21,
+    daysRemaining: 10
+  },
+  {
+    id: 5,
+    name: "Event Name: Lorem ipsum",
+    date: "January 10, 2026",
+    location: "123 Lorem ipsum dolor sit amet, Davao City, 8000",
+    progress: 21,
+    daysRemaining: 10
+  },
+  {
+    id: 6,
+    name: "Event Name: Lorem ipsum",
+    date: "January 10, 2026",
+    location: "123 Lorem ipsum dolor sit amet, Davao City, 8000",
+    progress: 21,
+    daysRemaining: 10
+  }
+];
