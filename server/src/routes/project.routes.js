fc3f923c529e41ef422b851e1d7411f8b72c823c
pebble_na_bato <feevolinto123@gmail.com>
2026-01-27 15:07:29 +0800

    Update project.routes.js

--- server/src/routes/project.routes.js
+++ server/src/routes/project.routes.js
@@ -1,10 +1,38 @@
 import express from "express";
-import { createProject, getMyProjects } from "../../controllers/projectController.js";
+import { 
+  createProject, 
+  getMyProjects,
+  getProjectById,
+  updateProject,
+  deleteProject
+} from "../controllers/projectController.js";  // ✅ Correct path
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+/**
+ * CREATE project
+ */
 router.post("/", authMiddleware, createProject);
+
+/**
+ * GET all my projects
+ */
 router.get("/", authMiddleware, getMyProjects);
+
+/**
+ * GET single project by ID
+ */
+router.get("/:id", authMiddleware, getProjectById);
+
+/**
+ * UPDATE project
+ */
+router.patch("/:id", authMiddleware, updateProject);
+
+/**
+ * DELETE project
+ */
+router.delete("/:id", authMiddleware, deleteProject);
 
 export default router;
