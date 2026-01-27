80f039c6c92a6e765bd1b67b7d5382b5b16246e7
pebble_na_bato <feevolinto123@gmail.com>
2026-01-27 15:07:22 +0800

    Update Project.js

--- server/src/models/Project.js
+++ server/src/models/Project.js
@@ -5,14 +5,39 @@ const projectSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+
   description: {
     type: String
   },
+
+  date: {
+    type: Date
+  },
+
+  location: {
+    type: String
+  },
+
+  leader: {
+    type: String
+  },
+
+  committee: {
+    type: String
+  },
+
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active"
+  },
+
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true
   },
+
   members: [
     {
       type: mongoose.Schema.Types.ObjectId,
