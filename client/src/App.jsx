de9b5af825190dcd114df3b22023b1772c04c2d8
pebble_na_bato <feevolinto123@gmail.com>
2026-01-30 01:32:55 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 // src/App.jsx
 import { Routes, Route, Navigate } from "react-router-dom";
-import AuthProvider from "./context/AuthContext"; 
+import { Toaster } from 'react-hot-toast';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import ProjectOverview from "./pages/ProjectOverview"; // Add this import
+import ProjectOverview from "./pages/ProjectOverview";
 import History from "./pages/History";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
@@ -12,56 +12,60 @@ import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
+    <>
+      {/* Toast Notifications */}
+      <Toaster />
+      
+      <Routes>
+        <Route path="/login" element={<Login />} />
 
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Layout>
-              <Home />
-            </Layout>
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Home />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Layout>
-              <Dashboard />
-            </Layout>
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-      {/* Add this new route */}
-      <Route
-        path="/dashboard/project/:projectId"
-        element={
-          <ProtectedRoute>
-            <Layout>
-              <ProjectOverview />
-            </Layout>
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/dashboard/project/:projectId"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <ProjectOverview />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/history"
-        element={
-          <ProtectedRoute>
-            <Layout>
-              <History />
-            </Layout>
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/history"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <History />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
 
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 }
 
